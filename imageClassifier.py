fd9b2fbc7009fefc6d3133753a336e4957810e07
Yuuraa <yoorachoi8937@gmail.com>
2020-09-25 23:15:38 +0900

    Image classifier tutorial done

--- imageClassifier.py
+++ imageClassifier.py
@@ -60,6 +60,10 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 net = Net()
+# Send the network to GPU
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+net.to(device)
 
 #### Loss function and Optimizer ####
 import torch.optim as optim
@@ -70,7 +74,7 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 for epoch in range(2):
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
+        inputs, labels = data[0].to(device), data[1].to(device)
         # zero the parameter gradients
         optimizer.zero_grad()
 
@@ -91,6 +95,53 @@ print('Finished Training')
 PATH = './cifar_net.pth'
 torch.save(net.state_dict(), PATH)
 
-# Test
+# Test the network on the test data
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+# print images
+# imshow(torchvision.utils.make_grid(images))
+# print('Grount Truth: ', ''.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# Predict the result and calculate the score of the network
+
 net = Net()
-net.load_state_dict(
+net.load_state_dict(torch.load(PATH))
+
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ''.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+correct = 0
+total = 0
+# TODO: no_grad 찾아보기
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' %(100 * correct / total))
+
+# Calculate accuracy for each classes
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        ouptuts = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        print(c)
+        # 한 batch에 있는 것들에 대해
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] = c[i].item()
+            class_total[label] += 1
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' %(classes[i], 100 * class_correct[i] / class_total[i]))
